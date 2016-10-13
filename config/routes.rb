66483b4e750c6a79ab92640656846467b43c5407
Roderick Fung <roderickfung@gmail.com>
2016-10-12 17:54:53 -0700

    presi version

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  default_url_options host: 'http://heartcompanion.herokuapp.com'
+  # default_url_options host: 'http://heartcompanion.herokuapp.com'
 
   root 'home#index'
   get '/about' => 'home#about', as: :about
