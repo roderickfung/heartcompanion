1ceb64143737cf4140cbf9aad54392e8ba53d9b4
Roderick Fung <roderickfung@gmail.com>
2016-10-17 16:51:47 -0700

    new port mailer fix

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,6 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   address:              "smtp.gmail.com",
-  port:                 "587",
+  # port:                 "587",
+  port:                 '465',
   enable_starttls_auto: true,
   authentication:       :plain,
   user_name:            ENV["EMAIL_USER_NAME"],
