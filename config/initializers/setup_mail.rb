2d5d02a6b606a763c8928753db9bf20d978c5c09
Roderick Fung <roderickfung@gmail.com>
2016-10-17 18:00:36 -0700

    more env variables mailer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  address:              "smtp.gmail.com",
+  address:              ENV['EMAIL_SERVER'],
   # port:                 "587",
   # port:                 '465',
   port:                 ENV["EMAIL_PORT"],
-  enable_starttls_auto: true,
-  authentication:       :plain,
+  enable_starttls_auto: ENV['EMAIL_TLS_AUTO'],
+  authentication:       ENV['EMAIL_AUTHENTICATION'],
   user_name:            ENV["EMAIL_USER_NAME"],
   password:             ENV["EMAIL_PASSWORD"]
 }
